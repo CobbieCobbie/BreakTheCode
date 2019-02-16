063f7cf2ab44405b8a4000880c2731d56d6287ec
cobbiecobbie <Benjamin.Coban@gmail.com>
2019-02-16 19:20:11 +0100

    try to add player constraint on camera, not finished!

--- BreakTheCode/Assets/Scripts/CameraController.cs
+++ BreakTheCode/Assets/Scripts/CameraController.cs
@@ -4,21 +4,43 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public float followSpeed = 4.0f;
+    //Screen constraints
+    private float screenWidth, screenHeight;
+    private Vector3 screenCenter;
+    private GameObject sceneCam;
+
+    public float followSpeed = 1.0f;
     //private Vector3 direction;
     public Transform player;
     public Transform aim;
     private Transform transform;
+    Vector2 screenBounds, screenOrigo;
+    
 
-    void start(){
+    void Start(){
         //direction = Vector3.zero;
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        screenOrigo = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+        screenCenter = new Vector3((screenBounds.x - screenOrigo.x) / 2 , (screenBounds.y - screenOrigo.y) / 2 , -10);
         transform = GetComponent<Transform>();
+        Debug.Log("Height: " + screenHeight  + ", Width: " + screenWidth);
     }
 
-    void LateUpdate(){
-        Vector3 direction = calcDirection();
-        Vector3 move = new Vector3(direction.x * followSpeed, direction.y * followSpeed, 0);
-        transform.Translate(move , Space.Self);
+    void Update(){
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        screenOrigo = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        screenCenter = new Vector3((screenBounds.x - screenOrigo.x) / 2 , (screenBounds.y - screenOrigo.y) / 2 , -10);
+
+        if (Vector3.Distance(transform.position,screenCenter) < ((screenBounds.y - screenOrigo.y) / 2))
+        {
+            Vector3 direction = calcDirection();
+            Vector3 move = new Vector3(direction.x * followSpeed, direction.y * followSpeed, -10);
+            move.Normalize();
+            transform.Translate(move * Time.deltaTime * followSpeed);
+        }
         //TODO mal anschauen --> transform.position = Vector3.SmoothDamp(transform.position,)
     }
 
@@ -31,7 +53,6 @@ public class CameraController : MonoBehaviour {
     }*/
 
     private Vector3 calcDirection(){
-        return (aim.position - player.position);
+        return new Vector3(aim.position.x - player.position.x,aim.position.y - player.position.y,0);
     }
-
 }
