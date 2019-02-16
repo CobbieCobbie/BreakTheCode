7fa8fec5b31476d7b67ae6da1f93bf27d6c883f0
cobbiecobbie <Benjamin.Coban@gmail.com>
2019-02-16 14:31:44 +0100

    update

--- BreakTheCode/Assets/Scripts/CameraController.cs
+++ BreakTheCode/Assets/Scripts/CameraController.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	//public GameObject player;
     private static float followSpeed = 4.0f;
-    private Vector3 offset;
+    public Vector3 offset;
+    private Vector3 cameraPosition;
+    private Transform player;
+    private Transform aim;
 
     void start(){
-    	offset = transform.position - player.transform.position;
+        offset = transform.position - player.transform.position;
+        cameraPosition = new Vector3(0,0,-10.0f);
     }
 
-    void LateUpdate()
+    /*void Update()
     {
-    	transform.position = player.transform.position + offset;
+        cameraPosition.x = player.transform.position.x;// + offset.x;
+        cameraPosition.y = player.transform.position.y;// + offset.y;
+        this.transform.position = cameraPosition;
+
+        //TODO mal anschauen --> transform.position = Vector3.SmoothDamp(transform.position,)
+    }*/
+
+    void LateUpdate(){
+        transform.position = player.transform.position + offset;
     }
 
 }
